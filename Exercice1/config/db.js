1665967616d02685ba68a3cfa7e6e48d9c0594bc
tcloud1105 <68422485+tcloud1105@users.noreply.github.com>
2020-10-10 10:08:38 +0200

    Add files via upload

---
+++ Exercice1/config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url : "mongodb://kayimfotso:12334@ds125146.mlab.com:25146/notable"
+}
---
+++ Exercice1/server.js
@@ -0,0 +1,19 @@
+const express     = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser  = require('body-parser');
+const db          = require('./config/db');
+
+const app         = express();
+const port        = 8000;
+
+app.use(bodyParser.urlencoded({extended:true}))// to parse the body of the request
+
+
+MongoClient.connect(db.url, (err, database)=>{
+    if(err) console.log(err)
+
+    require('./app/routes')(app, database)
+    app.listen(port , () => {
+        console.log('We are live on '+ port);
+    })
+})
